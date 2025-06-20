8fed1de421a9967a6dab7aaa64e7c0e9fc7010b6
oussema-by <oussemabenyahia89@gmail.com>
2025-06-20 02:03:54 +0100

    feat: add @vercel/analytics dependency and integrate analytics component in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-
+import { Analytics } from '@vercel/analytics/react'
 export const metadata: Metadata = {
   title: "Portfolio",
   description: "My porfolio",
@@ -15,7 +15,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
-  
+        <Analytics />
       </body>
     </html>
   );
